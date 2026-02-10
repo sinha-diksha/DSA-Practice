f219eeffe9828fd40d379f3106233c8edcf23756
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2026-02-11 02:20:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (7.58%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<int> st;
+        int n=s.length();
+        for(int i=0; i<n; i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }else{
+                if(!st.empty()){
+                    if((s[i]==']' && st.top()=='[')|| 
+                    (s[i]==')' && st.top()=='(') ||
+                    (s[i]=='}' && st.top()=='{')){
+                        st.pop();
+                    }else{
+                        st.push(s[i]);
+                    }
+                }else{
+                    st.push(s[i]);
+                }
+            }
+        }
+
+        return st.size();
+    }
+};
