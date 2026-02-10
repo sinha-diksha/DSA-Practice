bedbcdfdbe3bdf14faf9f7da454034443945b109
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2026-02-11 02:21:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (7.58%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -4,13 +4,11 @@ public:
         stack<int> st;
         int n=s.length();
         for(int i=0; i<n; i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+            if(s[i]=='('){
                 st.push(s[i]);
             }else{
                 if(!st.empty()){
-                    if((s[i]==']' && st.top()=='[')|| 
-                    (s[i]==')' && st.top()=='(') ||
-                    (s[i]=='}' && st.top()=='{')){
+                    if((s[i]==')' && st.top()=='(')){
                         st.pop();
                     }else{
                         st.push(s[i]);
