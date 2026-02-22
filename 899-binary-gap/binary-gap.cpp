2a959f561fec7f5b81727a0cf4078474bb15fbe1
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2026-02-22 18:18:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (44.66%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int firstBit=-1;
+        int ans=0;
+        int secondBit=-1;
+        for(int i=31; i>=0; i--){
+            if((n & (1<<i))!=0){
+                if(firstBit==-1){
+                    firstBit=i;
+                }else{
+                    secondBit=i;
+                    ans=max(ans, firstBit-secondBit);
+                    firstBit=secondBit;
+                }
+            }
+        }
+        if(firstBit==-1 || secondBit==-1){
+            return 0;
+        }
+        cout<<ans<<endl;
+        return ans;
+    }
+};
