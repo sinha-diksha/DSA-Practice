0e1640d2989f4965a2a5b1113fcb321784ba2aa1
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2026-04-08 22:58:45 +0530

    Time: 56 ms (78.47%) | Memory: 158.2 MB (35.41%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
@@ -5,12 +5,11 @@ public:
         int n=queries.size();
         int m=nums.size();
         for(int i=0; i<n; i++){
-            int k=queries[i][2];
             int v=queries[i][3];
             int idx=queries[i][0];
             while(idx<=queries[i][1]){
                 nums[idx]=((long)nums[idx] * v)%((long)1e9+7);
-                idx+=k;
+                idx+=queries[i][2];
             }
         }
 
