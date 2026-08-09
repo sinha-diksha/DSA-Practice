7e3aae41b99a928900a35e1235890df456017788
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2026-08-09 11:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (49.18%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -4,17 +4,14 @@ public:
         int n=nums.size();
         int l=1;
         int h=n-2;
-        while(l<=h){
+        while(l<h){
             int mid = l + (h-l)/2;
-            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
-                return mid;
-            }
             if(nums[mid]<nums[mid+1]){
                 l=mid+1;
             }else{
-                h=mid-1;
+                h=mid;
             }
         }
-        return -1;
+        return l;
     }
 };
