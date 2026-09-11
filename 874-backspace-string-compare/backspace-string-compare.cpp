cd2829861fe6bf27876ac4d5cb28e5eefe3b452b
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2026-09-12 01:27:48 +0530

    Time: 2 ms (10.49%) | Memory: 8.4 MB (85.69%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
-    bool backspaceCompare(string s, string t) {
+    string finalString(string s){
+        string temp="";
         int n=s.length();
-        int m=t.length();
-        int sCountHash=0;
-        int tCountHash=0;
-        int i=n-1;
-        int j=m-1;
-        while(i>=0 || j>=0){
-            while(i>=0){
-                if(s[i]=='#'){
-                    sCountHash++;
-                    i--;
-                }else if(sCountHash>0){
-                    sCountHash--;
-                    i--;
-                }else{
-                    break;
-                }
+        for(int i=0; i<n; i++){
+            if(s[i]!='#') temp.push_back(s[i]);
+            else if(temp.size()){
+                 temp.pop_back();
             }
-            while(j>=0){
-                if(t[j]=='#'){
-                    tCountHash++;
-                    j--;
-                }else if(tCountHash>0){
-                    tCountHash--;
-                    j--;
-                }else{
-                    break;
-                }
-            }
-            sCountHash=0;
-            tCountHash=0;
-            if(i<0 && j<0) return true;
-            if(i<0 || j<0) return false;
-            if(s[i]!=t[j]) return false;
-            i--; j--;
         }
-        return true;
+        return temp;
+    }
+    bool backspaceCompare(string s, string t) {
+       string temp1 = finalString(s);
+       string temp2 = finalString(t);
+       return temp1==temp2;
     }
 };
