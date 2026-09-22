b85e779546bd7c5654aa11eca889c1a95aa2d09e
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2026-09-23 01:14:13 +0530

    Time: 2 ms (22.27%) | Memory: 8.3 MB (36.28%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_map<int,int>mp;
+        while(n!=1){
+            if(mp.count(n)>0){
+                return false;
+            }
+            mp[n]=1;
+            int x=n;
+            int sum=0;
+            while(x>0){
+                int d = x%10;
+                sum+=(d*d);
+                x/=10;
+            }
+            if(sum==1){
+                return true;
+            }
+            n=sum;
+        }
+        return true;
+    }
+};
