6deb4d66452739d521e7a8c6dbaa4d9d72106057
sinha-diksha <118713975+sinha-diksha@users.noreply.github.com>
2026-09-27 00:40:01 +0530

    Time: 137 ms (6.89%) | Memory: 76.7 MB (14.09%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-       
-        int ans=0;
-        for(auto& it:nums){
-            mp[it]++;
-            int val =it;
-            int times= mp[it];
-            int target = k - val;
-            if(mp.count(target)){
-                if(val!=target){
-                    int v=min(times, mp[target]);
-                    mp[it]-=v;
-                    mp[target]-=v;
-                    ans+=v;
-                }else{
-                    int v=mp[target]/2;
-                    ans+=v;
-                    mp[target]-=(2*v);
-                }
+        unordered_map<int, int> mp;
+        int ans = 0;
+
+        for (int x : nums) {
+            int complement = k - x;
+
+            if (mp[complement] > 0) {
+                mp[complement]--;
+                ans++;
+            } else {
+                mp[x]++;
             }
         }
-
 
         return ans;
     }
